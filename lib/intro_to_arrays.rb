88a8210608824655e911980bd718799d24090031
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-04 16:51:09 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,29 @@
+def instantiate_new_array
+  
+   @my_new_array = []
+   return @my_new_array
+  
+end 
+
+
+
+def array_with_two_elements
+  @my_two_array = ["two", "elements"]
+  return @my_two_array
+
+end 
+
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+
+def first_element(taylor_swift)
+  my_first_element = taylor_swift[0]
+  return my_first_element
+  
+
+end 
+
+def third_element(taylor_swift)
+  my_third_element = taylor_swift[2]
+  return my_third_element
+  
+end 
